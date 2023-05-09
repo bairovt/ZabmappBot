aa9ae93905627fe4044cf33d31eba25600bffe91
Tumen <bairovt@gmail.com>
2023-05-09 10:09:38 +0900

    pm2 conf

---
+++ pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps : [{
+    name      : "ZabmappBot",
+    script    : "./bot.js",
+    cwd       : "/home/tumen/mapp/mapp75bot/build",
+    watch: false,
+    instance_var: "INSTANCE_ID",
+    env: {
+	NODE_ENV: "test",
+//	NODE_CONFIG_DIR: '/home/tumen/nodejs/omog-koa/config/',
+//	NODE_PATH: '/home/tumen/nodejs/omog-koa'
+    },
+//    source_map_support: false
+  }],
+};
